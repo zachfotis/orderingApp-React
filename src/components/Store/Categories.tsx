243a8a819d13388ffcbfafe1a28ba6b9b7fb0fff
Fotios Zachopoulos <65657774+zachfotis@users.noreply.github.com>
2023-01-20 16:36:28 +0200

    add basket in the ui

--- src/components/Store/Categories.tsx
+++ src/components/Store/Categories.tsx
@@ -7,13 +7,13 @@ function Categories({ catalog }: { catalog: object }) {
     window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' });
   };
   return (
-    <div className="w-full flex justify-start items-center gap-5 mt-5 px-5">
+    <div className="w-full flex justify-start items-center gap-5 p-5 overflow-x-auto">
       {Object.keys(catalog).map((key) => {
         return (
           <HashLink
             scroll={(el) => scrollWithOffset(el)}
             to={'#' + key}
-            className="min-w-[100px] text-center px-5 py-2 border border-1 border-black bg-white rounded-lg"
+            className="min-w-[100px] shrink-0 text-center px-5 py-2 border border-1 border-black bg-white rounded-lg"
             key={'categories-' + key}
           >
             {key}
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -8,11 +8,13 @@ import { baseURL } from '../utilities/server';
 import Menu from '../components/Store/Menu';
 
 import { Catalog } from '../types';
+import Basket from '../components/Basket';
 
 function Store() {
   const { id } = useParams<{ id: string }>();
   const { stores, setActiveStore } = useDeliveryContext();
   const [catalog, setCatalog] = useState<Catalog>({});
+  const [showBasket, setShowBasket] = useState(false);
 
   useEffect(() => {
     const getCatalog = async () => {
@@ -40,11 +42,19 @@ function Store() {
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
       exit={{ opacity: 0 }}
-      className="w-full max-w-[1000px] mx-auto flex flex-col justify-start items-start overflow-hidden"
+      className="w-full flex justify-start items-start gap-10"
     >
-      <Header store={store} isDeal={Object.hasOwnProperty.call(catalog, 'Specials')} />
-      <Categories catalog={catalog} />
-      <Menu catalog={catalog} />
+      <div
+        className={`relative w-full max-w-[800px] xl:max-w-[950px] mx-auto flex flex-col justify-start items-start mt-2 md:mt-5
+      ${showBasket ? 'xl:-translate-x-[150px]' : 'translate-x-0'}
+      transition-all duration-1000 ease-in-out
+      `}
+      >
+        <Header store={store} isDeal={Object.hasOwnProperty.call(catalog, 'Specials')} />
+        <Categories catalog={catalog} />
+        <Menu catalog={catalog} />
+      </div>
+      <Basket showBasket={showBasket} setShowBasket={setShowBasket} />
     </motion.div>
   );
 }
